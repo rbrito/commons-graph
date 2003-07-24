d3216151997265f95cf5a6be28de50c641652744
ddp <ddp@13f79535-47bb-0310-9956-ffa450edef68>
2003-07-24 12:35:15 +0000

    Tomasz Skutnik's idea.
    Here is VisitorAdapter class, which is dummy (no operation) 
    implementation of Visitor interface. You know what is it for :)
    Please commit.
    
    
    git-svn-id: http://svn.apache.org/repos/asf/jakarta/commons/sandbox/graph2/trunk@144644 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/graph/algorithm/search/VisitorAdapter.java
@@ -0,0 +1,109 @@
+package org.apache.commons.graph.algorithm.search;
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Commons" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Commons", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+import org.apache.commons.graph.Edge;
+import org.apache.commons.graph.Graph;
+import org.apache.commons.graph.Vertex;
+
+/**
+ * @author dixon-pd
+ *
+ * This is a do-nothing implementation of the
+ * Visitor class.  Inherit from here if you would
+ * like to implement Visitor, but really have no
+ * need for some of the methods.  (Or if you have
+ * no need for another superclass. . .)
+ */
+public class VisitorAdapter implements Visitor {
+	
+	public VisitorAdapter() {}
+	
+	/**
+	 * @see org.apache.commons.graph.algorithm.search.Visitor#discoverGraph(Graph)
+	 */
+	public void discoverGraph(Graph graph) {
+	}
+
+	/**
+	 * @see org.apache.commons.graph.algorithm.search.Visitor#discoverVertex(Vertex)
+	 */
+	public void discoverVertex(Vertex vertex) {
+	}
+
+	/**
+	 * @see org.apache.commons.graph.algorithm.search.Visitor#discoverEdge(Edge)
+	 */
+	public void discoverEdge(Edge edge) {
+	}
+
+	/**
+	 * @see org.apache.commons.graph.algorithm.search.Visitor#finishEdge(Edge)
+	 */
+	public void finishEdge(Edge edge) {
+	}
+
+	/**
+	 * @see org.apache.commons.graph.algorithm.search.Visitor#finishVertex(Vertex)
+	 */
+	public void finishVertex(Vertex vertex) {
+	}
+
+	/**
+	 * @see org.apache.commons.graph.algorithm.search.Visitor#finishGraph(Graph)
+	 */
+	public void finishGraph(Graph graph) {
+	}
+
+}
